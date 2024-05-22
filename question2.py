1b4ff7a674f1d566e6320dceb5e29246b0c1321a
sujithraja7 <155076732+sujithraja7@users.noreply.github.com>
2024-05-22 10:06:23 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,6 @@
 # find if the given number is a palindrome or not?
+num = input("Enter a number: ")
+if num == num[::-1]:
+    print(f"{num} is a palindrome")
+else:
+    print(f"{num} is not a palindrome")
