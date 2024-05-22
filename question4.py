ceb333bd7d21e882b2e5e1f0ddb16e0b269454e1
sujithraja7 <155076732+sujithraja7@users.noreply.github.com>
2024-05-22 10:19:18 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,10 @@
 #write a program to find the sum of digits of a given number'
+def sum_of_digits(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        total += digit
+        number //= 10
+    return total
+number = 687
+print("Sum of digits:", sum_of_digits(number)) 
